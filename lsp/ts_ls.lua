d87520b3410babb90fbe88ed1bef152139ad303c
Allen <wukuohao2022@icloud.com>
2025-12-02 21:00:49 +0800

    -

--- lsp/ts_ls.lua
+++ lsp/ts_ls.lua
@@ -1,15 +1,15 @@
 return {
-	init_options = { hostInfo = 'neovim' },
-	filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" }
+	init_options = { hostInfo = "neovim" },
+	filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
 	root_dir = function(bufnr, on_dir)
-		local root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' }
-		root_markers = vim.fn.has('nvim-0.11.3') == 1 and { root_markers, { '.git' } }
-		  or vim.list_extend(root_markers, { '.git' })
-		local deno_path = vim.fs.root(bufnr, { 'deno.json', 'deno.jsonc', 'deno.lock' })
+		local root_markers = { "package-lock.json", "yarn.lock", "pnpm-lock.yaml", "bun.lockb", "bun.lock" }
+		root_markers = vim.fn.has("nvim-0.11.3") == 1 and { root_markers, { ".git" } }
+			or vim.list_extend(root_markers, { ".git" })
+		local deno_path = vim.fs.root(bufnr, { "deno.json", "deno.jsonc", "deno.lock" })
 		local project_root = vim.fs.root(bufnr, root_markers)
 		if deno_path and (not project_root or #deno_path >= #project_root) then
-		  return
+			return
 		end
 		on_dir(project_root or vim.fn.getcwd())
-	  end,
+	end,
 }
--- lua/pkgs/telescope.config.lua
+++ lua/pkgs/telescope.config.lua
@@ -8,42 +8,42 @@ return {
 		"OriginCoderPulse/telescope-node-packages.nvim",
 	},
 	config = {
-			defaults = {
-				preview = false,
-				layout_config = {
-					width = 0.5,
-					height = 0.6,
-					prompt_position = "top",
-				},
-				history = {
-					path = os.getenv("HOME") .. "/.nvim-utils/telescope/telescope-history",
-				},
-				path_display = {
-					"tail",
-				},
-				file_ignore_patterns = {
-					".git",
-					".hg",
-					".DS_Store",
-					"__pycache__",
-					"%.log",
-				},
-				extensions = {
-					live_grep_args = {},
-					treesitter_info = {},
-					node_packages = {},
-					codecompanion = {},
-					git_branch = {
-						opts = {
-							layout_config = {
-								width = 0.8,
-								height = 0.9,
-								prompt_position = "bottom",
-							},
+		defaults = {
+			preview = false,
+			layout_config = {
+				width = 0.5,
+				height = 0.6,
+				prompt_position = "top",
+			},
+			history = {
+				path = os.getenv("HOME") .. "/.nvim-utils/telescope/telescope-history",
+			},
+			path_display = {
+				"tail",
+			},
+			file_ignore_patterns = {
+				".git",
+				".hg",
+				".DS_Store",
+				"__pycache__",
+				"%.log",
+			},
+			extensions = {
+				live_grep_args = {},
+				treesitter_info = {},
+				node_packages = {},
+				codecompanion = {},
+				git_branch = {
+					opts = {
+						layout_config = {
+							width = 0.8,
+							height = 0.9,
+							prompt_position = "bottom",
 						},
 					},
 				},
-				sorting_strategy = "ascending",
 			},
+			sorting_strategy = "ascending",
+		},
 	},
 }
